b00e9929a2841daad46b601519fabe788c52fb12
Roman Shupik <romanwupik@gmail.com>
2024-09-16 13:34:38 +0300

    09.09.2024 solution
    Ffrom decimal to other notations

---
+++ 09.09.2024/mainfile.c
@@ -0,0 +1,44 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+
+int a, b, max_number, mod, counter = 1;
+
+int main(void) {
+m1:
+    printf("Enter a(0-100000), b(0-32): ");
+    int n = scanf("%d, %d", &a, &b);
+    if (n < 2) {
+        printf("You must enter 2 numbers!\n\n");
+        goto m1;
+    }
+    int a_copy = a;
+
+m2:
+    a_copy /= b;
+    if (a_copy != 0) {
+        max_number += 1;
+        goto m2;
+    }
+    a_copy = a;
+    mod = a_copy % b;
+m3:
+    if (counter <= max_number) {
+        a_copy /= b;
+        mod = a_copy % b;
+        counter += 1;
+        goto m3;
+    }
+
+    printf("%d ", mod);
+    max_number -= 1;
+    counter = 1;
+    a_copy = a;
+    mod = a_copy % b;
+    if (max_number >= 0) {
+        goto m3;
+    }
+
+    return 0;
+}
